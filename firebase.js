f49a1dd9b66528b3e73428e4c7b7b4c2559f97eb
ANURAG-2043 <96662574+ANURAG-2043@users.noreply.github.com>
2024-05-03 14:27:15 +0530

    contact form linked to mail successfully and firebase code removed

--- firebase.js
+++ firebase.js
@@ -1,54 +1,54 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDfooMN5vxR2fRYCT3POQpEk6pe56uiBio",
-    authDomain: "minerva-1604.firebaseapp.com",
-    databaseURL: "https://minerva-1604-default-rtdb.firebaseio.com",
-    projectId: "minerva-1604",
-    storageBucket: "minerva-1604.appspot.com",
-    messagingSenderId: "399734462747",
-    appId: "1:399734462747:web:5d52eace3f283930cbdbbe",
-    measurementId: "G-83GZN4GD8Y"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDfooMN5vxR2fRYCT3POQpEk6pe56uiBio",
+//     authDomain: "minerva-1604.firebaseapp.com",
+//     databaseURL: "https://minerva-1604-default-rtdb.firebaseio.com",
+//     projectId: "minerva-1604",
+//     storageBucket: "minerva-1604.appspot.com",
+//     messagingSenderId: "399734462747",
+//     appId: "1:399734462747:web:5d52eace3f283930cbdbbe",
+//     measurementId: "G-83GZN4GD8Y"
+//   };
 
   
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-var contactFormDB = firebase.database().ref("contactForm");
+// var contactFormDB = firebase.database().ref("contactForm");
 
-document.getElementById("contactForm").addEventListener("submit", submitForm);
+// document.getElementById("contactForm").addEventListener("submit", submitForm);
 
-function submitForm(e){
-    e.preventDefault();
+// function submitForm(e){
+//     e.preventDefault();
 
-    var name = getElementVal("name");
-    var email = getElementVal("email");
-    var phone = getElementVal("phone");
-    var message = getElementVal("message");
-
-
-    saveMessage(name, email, phone, message);
-
-    document.querySelector(".alert").style.display = "block";
-
-    setTimeout(()=>{
-        document.querySelector(".alert").style.display = "none";
-    },3000);
-
-    document.getElementById("contactForm").reset();
-
-} 
+//     var name = getElementVal("name");
+//     var email = getElementVal("email");
+//     var phone = getElementVal("phone");
+//     var message = getElementVal("message");
 
 
-const saveMessage = (name, email, phone, message) =>{
-    var newContactForm = contactFormDB.push();
+//     saveMessage(name, email, phone, message);
 
-    newContactForm.set({
-        name: name,
-        email: email,
-        phone: phone,
-        message: message,
-    })
-};
+//     document.querySelector(".alert").style.display = "block";
 
-const getElementVal = (id) => {
-    return document.getElementById(id).value;
-};
+//     setTimeout(()=>{
+//         document.querySelector(".alert").style.display = "none";
+//     },3000);
+
+//     document.getElementById("contactForm").reset();
+
+// } 
+
+
+// const saveMessage = (name, email, phone, message) =>{
+//     var newContactForm = contactFormDB.push();
+
+//     newContactForm.set({
+//         name: name,
+//         email: email,
+//         phone: phone,
+//         message: message,
+//     })
+// };
+
+// const getElementVal = (id) => {
+//     return document.getElementById(id).value;
+// };
